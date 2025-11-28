47ac203e740776b8fd37ed1dd80770f0df60e110
golmannic <golmannic@gmail.com>
2025-11-28 14:00:23 -0700

    Created POST, Created Course search, update, delete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from pydantic import BaseModel, Field
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException
@@ -41,10 +41,10 @@ Base.metadata.create_all(bind=engine)
 
 class UserCreate(BaseModel):
     username: str
-    password: str = Field(ax_length=72)
+    password: str = Field(max_length=72)
 
 
-class ClassCreate(BaseModel):
+class CourseCreate(BaseModel):
     title: str
     professor: str
     description: str
@@ -79,4 +79,71 @@ def register(user: UserCreate, db: SessionLocal = Depends(get_db)): # type: igno
 
     return {"message": "User created successfully"}
 
+# GET all courses
+@app.get("/courses")
+def get_courses(db: Session = Depends(get_db)):
+    courses = db.query(Course).all()
+    return courses
 
+
+#CREATE courses
+@app.post("/courses")
+def create_course(course: CourseCreate, db: Session = Depends(get_db)):
+
+    existing = db.query(Course).filter(Course.title == course.title).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Course already exists")
+    
+    new_course = Course(
+        title=course.title,
+        professor=course.professor,
+        description=course.description
+    )
+
+    db.add(new_course)
+    db.commit()
+    db.refresh(new_course)
+
+    return {"message": "Course created successfully", "course": new_course}
+
+
+
+# GET single course with ID
+@app.get("/course/{course_id}")
+def get_course(course_id: int, db: Session = Depends(get_db)):
+    course = db.query(Course).filter(Course.id == course_id).first()
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return course
+
+
+# UPDATE course with ID
+@app.put("/courses/{course_id}")
+def update_course(course_id: int, updated: CourseCreate, db: Session = Depends(get_db)):
+    
+    course = db.query(Course).filter(Course.id == course_id).first()
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found")
+    
+    course.title = updated.title
+    course.professor = updated.professor
+    course.description = updated.description
+
+    db.commit()
+    db.refresh(course)
+
+    return{"message": "Course was Created", "course": course}
+
+
+# DELETE a course
+@app.delete("/course/{course_id}")
+def delete_course(course_id: int, db: Session = Depends(get_db)):
+
+    course = db.query(Course).filter(Course.id == course_id).first()
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found")
+    
+    db.delete(course)
+    db.commit()
+
+    return{"message": "Course deleted successfully"}
